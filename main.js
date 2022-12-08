a254d2a20bb4c63796a3cae7f595656b3936d665
Emmanueladeyemi123 <96444111+Emmanueladeyemi123@users.noreply.github.com>
2022-12-08 19:22:56 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 var img = "";
 var status="";
-leftWristX=0;
-leftWristY=0;
-rightWristX=0;
-rightWristY=0;
 function preload() {
-   img = loadImage("dog_cat.jpg");
+   img = loadImage("tj.jpeg");
 }
 
 function setup() {
@@ -14,23 +10,20 @@ function setup() {
 
    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
    document.getElementById("status").innerHTML="Status: Detecting object";
-   
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on("pose", gotPoses);
 }
 
 function draw() {
    image(img, 0, 0, 640, 420);
    fill("green");
    stroke("green");
-   text("Dog", 45, 75);
+   text("Steve", 45, 75);
    noFill();
-   rect(40, 60, 280, 300);
+   rect(40, 50, 280, 350);
    fill("red");
    stroke("red");
-   text("Cat", 400, 75);
+   text("Alex", 400, 75);
    noFill();
-   rect(300, 60, 280, 300);
+   rect(300, 50, 280, 350);
 
 }
 function modelLoaded(){
@@ -43,11 +36,4 @@ function gotResult(error,results){
       console.log(error);
    }
    console.log(results);
-   if(results>=0){
-leftWristX=results[0].pose.leftWrist[0].x;
-leftWristY=results[0].pose.leftWrist[0].y;
-rightWristX=results[0].pose.rightWrist[0].x;
-rightWristY=results[0].pose.rightWrist[0].y;
-
-   }
 }
